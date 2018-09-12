5f0d0a4ef81f2690a331cd5a151fb79ab0cac14a
matthunterking <matthew85king@gmail.com>
2018-09-12 12:14:27 +0100

    Set up basic player movement

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 console.log('connected!');
+
+const keys = { 38: ['top', '-'], 40: ['top', '+'], 37: ['left', '-'], 39: ['left', '+'] };
+const $player = $('<div class="playerFish"></div>');
+const playerPostion = { top: 300, left: 300 };
+
+function setUp() {
+  //Create a player div - Set size, add controls/movement
+  createPlayer();
+  //Create a gameboard
+  //Randomly generate other Fish
+}
+
+function createPlayer() {
+  $('body').append($player);
+  addControls();
+}
+
+function addControls() {
+  $(window).keydown((e) => {
+    const direction = keys[e.which];
+    movePlayer(direction);
+  });
+}
+
+function movePlayer(direction) {
+  direction[1] === '+' ? playerPostion[direction[0]] += 3 : playerPostion[direction[0]] -= 3;
+  console.log('in move', playerPostion);
+  $player.css(`${direction[0]}`, `${playerPostion[direction[0]]}px`);
+}
+
+//Check for collision - if collision then compare the sizes of Fish
+
+//Function to check if game over or increase the player size - Remove fish from game
+
+//Function to increase player size
+
+$(function(){
+  setUp();
+});
